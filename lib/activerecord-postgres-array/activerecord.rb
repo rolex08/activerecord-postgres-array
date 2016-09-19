d3508a4be44a92c1b325447892d6dd996f0b1f8e
rolex08 <kustikova@ek.apress.ru>
2016-09-19 19:10:10 +0500

    fix: undefined method 'to_hstore' for {}:Hash

--- lib/activerecord-postgres-array/activerecord.rb
+++ lib/activerecord-postgres-array/activerecord.rb
@@ -17,8 +17,6 @@ module ActiveRecord
             if column.type.to_s =~ /_array$/ && value && value.is_a?(Array)
               use_prepared_statements = self.class.connection_config.fetch(:prepared_statements, true)
               value = value.to_postgres_array(new_record? && use_prepared_statements)
-            elsif defined?(::Hstore) && column.type == :hstore && value && value.is_a?(Hash)
-              value = value.to_hstore
             elsif !value.nil? && coder = klass.serialized_attributes[name]
               value = coder.dump value
             end
